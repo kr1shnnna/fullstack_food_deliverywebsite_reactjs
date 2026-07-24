596994b77dc5d7a3c2247466c63edabf1ee5cfb6
Krishna <krishna9842740994@gmail.com>
2026-07-24 09:01:09 +0530

    work in progress

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,17 @@
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1> Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring  a delectable array of dishes. Our mission is to full your tummy with joy and happiness </p>
+        <div className='explore-menu-list'>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default ExploreMenu
