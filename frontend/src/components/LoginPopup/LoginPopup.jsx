1af82c28f8d22be4e3727fb9e4c614b97d468708
Krishna <krishna9842740994@gmail.com>
2026-07-26 16:18:21 +0530

    user auth fully baked

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,12 @@
 import './LoginPopup.css'
 import { useState } from 'react'  
-import {assets} from '../../assets/assets' 
+import {assets} from '../../assets/assets'
+import { useContext } from 'react'
+import { StoreContext } from '../../Context/StoreContext'
+import axios from 'axios'
 
 const LoginPopup = ({setShowLogin}) => {
+  const {url,setToken}=useContext(StoreContext)
 
     const [currState,setCurrState] = useState('Login')
 
@@ -22,10 +26,35 @@ const LoginPopup = ({setShowLogin}) => {
       }));
     }
 
+    const onLogin=async(event)=>{
+
+      event.preventDefault();
+      let newUrl=url;
+      if(currState==='Login'){
+        newUrl+='/api/user/login'
+      }
+      else{
+        newUrl+='/api/user/register'
+      }
+
+      const response=await axios.post(newUrl,data); 
+
+      if(response.data.success){
+          setToken(response.data.token);
+          localStorage.setItem('token',response.data.token);
+          setShowLogin(false);
+      }
+
+      else{
+        alert(response.data.message);
+      }
+
+    }
+
    
   return (
     <div className='login-popup'>
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
             <h2>{currState}</h2>
             <img  onClick={()=>setShowLogin(false)}src={assets.cross_icon}></img>
@@ -35,7 +64,7 @@ const LoginPopup = ({setShowLogin}) => {
             <input name='email' onChange={onChangeHandler} value={data.email}type='email' placeholder='Your Email' required/>
             <input name='password' onChange={onChangeHandler} value={data.password} type='password' placeholder='Password' required/>
         </div>
-        <button>{currState==='Sign Up'?'Create Account':'Login'}</button>
+        <button type='submit'>{currState==='Sign Up'?'Create Account':'Login'}</button>
         <div className="login-popup-condition">
             <input type='checkbox' required />
             <p>By continuing , i agree to the terms of use & privacy policy .</p>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,11 +4,23 @@ import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { StoreContext } from '../../Context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu]=useState("home");
 
-    const {getTotalCartAmount} = useContext(StoreContext)
+
+
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext)
+
+    const navigate= useNavigate();
+
+    const logout=()=>{
+localStorage.removeItem('token');
+setToken('');
+navigate('/')
+
+    }
   return (
     <div className='navbar'>
        <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
@@ -23,8 +35,24 @@ const Navbar = ({setShowLogin}) => {
             <div className="navbar-search-icon">
                 <Link to='/cart'>  <img src={assets.basket_icon}/></Link>  
                 <div className={getTotalCartAmount()===0?'':'dot'}></div>
-            </div>
-            <button onClick={()=>setShowLogin(true)}>Sign In</button>
+            </div> 
+            {
+            !token?<button onClick={()=>setShowLogin(true)}>Sign In</button>
+            :<div className='navbar-profile'>
+                <img src={assets.profile_icon} />
+                <ul className='navbar-profile-dropdown'>
+                    <li>
+                        <img src={assets.bag_icon}/>
+                        <p>Orders</p>
+                    </li>
+                    <hr />
+                    <li onClick={logout}>
+                       <img src={assets.logout_icon}/>
+                       <p>Logout</p>       
+                    </li>
+                </ul>
+                </div>} 
+            
         </div>
  
     </div>
