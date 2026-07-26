37c15e74d5f94426255c08efef8a75edecef4125
Krishna <krishna9842740994@gmail.com>
2026-07-26 22:16:22 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 import 'dotenv/config'
 
 //app config
@@ -25,6 +26,7 @@ app.use("/api/food",foodRouter);
 app.use("/images",express.static("uploads"));
 app.use('/api/user',userRouter);
 app.use('/api/cart',cartRouter);
+app.use('/api/placeorder',orderRouter);
 
 
 app.get("/",(req,res)=>{
