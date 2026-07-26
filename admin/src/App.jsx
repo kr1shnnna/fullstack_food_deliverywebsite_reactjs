61b28c5222743a0f5668e492732a82a6b1f7d623
Krishna <krishna9842740994@gmail.com>
2026-07-26 11:12:44 +0530

    added navbar & sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+      </div>
+    </div>
   )
 }
 
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+
+import { assets } from '../../assets/assets'
+import './Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img   src={assets.add_icon}/>
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img   src={assets.order_icon}/>
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img   src={assets.order_icon}/>
+                <p>Orders</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
